fbf75b150cd27fb3e82b3069621c50d07c496ea7
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-24 21:03:28 +0530

    Added: Employee Wage Builder UC10

---
+++ EmpWageUC10.java
@@ -0,0 +1,72 @@
+
+public class EmpWageUC10 {
+	
+	public static final int IS_Part_Time = 1;
+    public static final int IS_FULL_TIME =2;
+	
+	private  int numOfCompany=0;
+	private companyEmpWage[] companyEmpWageArray;
+	
+	//parameterised constructor for initializing members of a class
+	public EmpWageUC10()
+	{
+		companyEmpWageArray = new companyEmpWage[5];
+		
+	}
+	
+	//Method to create an object and add details of multiple companies
+	public void addCompanyEmpWage(String company,int empRate,int workingDays,int maxHrs)
+	{
+		companyEmpWageArray[numOfCompany]=new companyEmpWage(company, empRate, workingDays, maxHrs);
+		numOfCompany++;
+	}
+	
+	//Static method to compute the wage for a particular company
+	private void computeEmpWage()
+	{
+		for(int i=0;i<numOfCompany;i++)
+		{
+			companyEmpWageArray[i].setTotalEmpWage(this.computingEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+	
+	private int computingEmpWage(companyEmpWage companyEmpWage)         
+	{                                                                                             
+		//variables                                                                               
+	    int empHrs = 0;                                                                           
+	    int empWage = 0, totalEmpWage = 0, totalWorkHrs = 0, totalWorkingDays =0;                 
+	    //Computation                                                                             
+	    while (totalWorkHrs <= companyEmpWage.maxHrs && totalWorkingDays < companyEmpWage.workingDays)                          
+	    {                                                                                         
+	    	totalWorkingDays++;                                                                   
+	    	int empcheck= (int) Math.floor(Math.random() * 10) % 3;                               
+	    	switch(empcheck)                                                                      
+	    	{                                                                                     
+	     		case IS_Part_Time: {                                                              
+	     								empHrs = 8;                                                                      
+	     								break;                                                    
+	     							}                                                             
+	     		case IS_FULL_TIME: {                                                              
+	     								empHrs = 4;                                                                      
+	     								break;                                                    
+	     							}                                                             
+	     		default: empHrs = 0;                                                              
+	    	}//end of switch                                                                      
+	    	totalWorkHrs += empHrs;                                                                                                    
+	    	totalEmpWage = companyEmpWage.empRate + totalWorkHrs;                                                               
+	    }// end of while loop                                                                     
+	    return totalEmpWage;
+	}                                                                                             
+
+	public static void main(String[] args) {
+		EmpWageUC10 employeeWageBuilder =new EmpWageUC10();
+		
+		employeeWageBuilder.addCompanyEmpWage("DMart",20,20,100);
+		employeeWageBuilder.addCompanyEmpWage("TATA Electronics",10,20,150);
+		employeeWageBuilder.addCompanyEmpWage("Deloitte",30,20,100);
+		employeeWageBuilder.computeEmpWage();             
+
+	}
+
+}
