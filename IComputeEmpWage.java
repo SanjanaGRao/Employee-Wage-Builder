153d0039ca88cbc2f831fcb7dbf5910250344cb1
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-25 20:03:37 +0530

    Modified: For UC14 - called getTotalWage

--- IComputeEmpWage.java
+++ IComputeEmpWage.java
@@ -3,4 +3,5 @@ public interface IComputeEmpWage {
 	
 	public void addCompanyEmpWage(String company,int empRate,int workingDays,int maxHrs);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 }
